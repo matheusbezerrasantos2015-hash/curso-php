46e4618c62c472b7335e5a3185ac04d9006fb2b0
Matheus Bezerra <matheusbezerrasantos2015@gmail.com>
2026-08-08 10:04:27 -0300

    first files

---
+++ inicio-php/variaveis.php
@@ -0,0 +1,12 @@
+<?php
+
+    $nomeDaVariavel = "Ola mundo"; // string, varchar, char
+    $numero = 89; // integer, number, int
+    $numeroDecimal = 8.6; // float, decimal, double
+    $liga = true; // boolean
+
+    print $nomeDaVariavel;
+    echo $numero;
+
+
+?>
